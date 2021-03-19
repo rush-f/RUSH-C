1c459a6fe48af737469706c56ce21e0d40fc0b62
khdscor <khds_cor@naver.com>
2021-03-19 15:59:10 +0900

    init: 백엔드 빈 프로젝트 생성

---
+++ backend/rush/src/main/java/rush/rush/RushApplication.java
@@ -0,0 +1,13 @@
+package rush.rush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RushApplication.class, args);
+    }
+
+}
---
+++ backend/rush/src/main/java/rush/rush/test.java
@@ -0,0 +1,14 @@
+package rush.rush;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @GetMapping("/test")
+    public String hello(){
+
+        return "dasdasdsadas";
+    }
+}
