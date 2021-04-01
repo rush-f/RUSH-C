4825a20e5cce577558fba99c3286c3aaf8971cec
김예림 <kiel0103@naver.com>
2021-04-01 14:50:58 +0900

    refactor: 전역변수를 함수 안에 넣음

--- frontend/rush/src/App.js
+++ frontend/rush/src/App.js
@@ -1,9 +1,9 @@
-import MapWithAMarker from "./components/Map";
+import Map from "./components/Map";
 import {CLIENT_ID} from "./constants/GoogleMapAuth";
 
 function App() {
   return (<>
-    <MapWithAMarker googleMapURL={CLIENT_ID}
+    <Map googleMapURL={CLIENT_ID}
       loadingElement={<div style={{height: `100%`}}/>}
       containerElement={<div style={{height: `400px`}}/>}
       mapElement={<div style={{height: `100%`}}/>}
--- frontend/rush/src/components/Map.js
+++ frontend/rush/src/components/Map.js
@@ -7,58 +7,58 @@ import {
 import MarkerClusterer
   from "react-google-maps/lib/components/addons/MarkerClusterer";
 
-const positions = [
-  {lat: 37.554722, lng: 126.970833},
-  {lat: 37.55, lng: 126.970833},
-  {lat: 37.54, lng: 126.970833},
-  {lat: 37.53, lng: 126.970833},
-  {lat: 37.52, lng: 126.970833},
-  {lat: 37.51, lng: 126.970833},
-  {lat: 37.50, lng: 126.970833},
-  {lat: 37.49, lng: 126.970833},
-  {lat: 37.48, lng: 126.970833},
-  {lat: 37.47, lng: 126.970833},
-  {lat: 37.46, lng: 126.970833},
-  {lat: 37.45, lng: 126.970833},
-  {lat: 37.554722, lng: 128.970833},
-  {lat: 37.60, lng: 128.970833},
-  {lat: 37.54, lng: 128.970833},
-  {lat: 37.53, lng: 128.970833},
-  {lat: 37.52, lng: 128.970833},
-  {lat: 37.51, lng: 128.970833},
-  {lat: 37.50, lng: 128.970833},
-  {lat: 37.49, lng: 128.970833},
-  {lat: 37.48, lng: 128.970833},
-  {lat: 37.47, lng: 128.970833},
-  {lat: 37.46, lng: 128.970833},
-  {lat: 37.45, lng: 128.970833},
-];
+const Map = withScriptjs(withGoogleMap((props) => {
+  const positions = [
+    {lat: 37.554722, lng: 126.970833},
+    {lat: 37.55, lng: 126.970833},
+    {lat: 37.54, lng: 126.970833},
+    {lat: 37.53, lng: 126.970833},
+    {lat: 37.52, lng: 126.970833},
+    {lat: 37.51, lng: 126.970833},
+    {lat: 37.50, lng: 126.970833},
+    {lat: 37.49, lng: 126.970833},
+    {lat: 37.48, lng: 126.970833},
+    {lat: 37.47, lng: 126.970833},
+    {lat: 37.46, lng: 126.970833},
+    {lat: 37.45, lng: 126.970833},
+    {lat: 37.554722, lng: 128.970833},
+    {lat: 37.60, lng: 128.970833},
+    {lat: 37.54, lng: 128.970833},
+    {lat: 37.53, lng: 128.970833},
+    {lat: 37.52, lng: 128.970833},
+    {lat: 37.51, lng: 128.970833},
+    {lat: 37.50, lng: 128.970833},
+    {lat: 37.49, lng: 128.970833},
+    {lat: 37.48, lng: 128.970833},
+    {lat: 37.47, lng: 128.970833},
+    {lat: 37.46, lng: 128.970833},
+    {lat: 37.45, lng: 128.970833},
+  ];
 
-const markers = positions.map((position, index) => <Marker
-  key={index}
-  position={position}
-  animation={1}
-  onClick={() => {
-    alert(index);
-  }}
-/>);
+  const markers = positions.map((position, index) => <Marker
+    key={index}
+    position={position}
+    animation={1}
+    onClick={() => {
+      alert(index);
+    }}
+  />);
 
-const MapWithAMarker = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={6}
-    defaultCenter={{lat: 37.397, lng: 127.644}}
-  >
-    <MarkerClusterer
-      onClick={props.onMarkerClustererClick}
-      averageCenter
-      enableRetinaIcons
-      gridSize={20}
-      minimumClusterSize={4}
+  return (
+    <GoogleMap
+      defaultZoom={6}
+      defaultCenter={{lat: 37.397, lng: 127.644}}
     >
-      {markers}
-    </MarkerClusterer>
+      <MarkerClusterer
+        onClick={props.onMarkerClustererClick}
+        averageCenter
+        enableRetinaIcons
+        gridSize={20}
+        minimumClusterSize={4}
+      >
+        {markers}
+      </MarkerClusterer>
+    </GoogleMap>);
+}));
 
-  </GoogleMap>
-))
-
-export default MapWithAMarker;
+export default Map;
