a1ea33c3d3ecd7c48eedc71aa456c358a61ae7ff
khdscor <khds_cor@naver.com>
2021-04-08 15:25:57 +0900

    feat: 글쓰기 step1 - 마커를 지도 중앙에 고정

--- frontend/rush/src/components/WritingMap.js
+++ frontend/rush/src/components/WritingMap.js
@@ -8,7 +8,9 @@ import {
 import {useState} from "react";
 
 const WritingMap = withScriptjs(withGoogleMap((props) => {
-  const [position, setPosition] = useState({lat: 37.397, lng: 127.644});
+  const [center, setCenter] = useState({lat: null, lng: null});
+
+  const [map, setMap] = useState(null);
 
   const defaultMapOptions = {
     disableDefaultUI: true
@@ -17,21 +19,19 @@ const WritingMap = withScriptjs(withGoogleMap((props) => {
   return (
       <>
         <GoogleMap
+            ref={(map) => setMap(map)}
             defaultZoom={6}
             defaultCenter={{lat: 37.397, lng: 127.644}}
             defaultOptions={defaultMapOptions}
-            onClick={(e) => {
-              setPosition({lat: e.latLng.lat(), lng: e.latLng.lng()});
+            streetView
+            onCenterChanged={() => {
+              setCenter(map.getCenter())
             }}
         >
           <Marker
-              position={position}
-              animation={1}
+              position={center}
               icon={{
                 url: '/footprint.png',
-              }}
-              onClick={() => {
-                alert();
               }}
           />
         </GoogleMap>
