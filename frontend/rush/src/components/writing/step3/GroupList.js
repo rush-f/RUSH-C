70e4a627b329dde8e895c0987b61f16a55d2e1a7
김예림 <kiel0103@naver.com>
2021-04-09 17:36:25 +0900

    feat: 글쓰기 step3 - 체크리스트 관리 구현

--- frontend/rush/src/components/writing/step3/GroupList.js
+++ frontend/rush/src/components/writing/step3/GroupList.js
@@ -7,20 +7,28 @@ const GroupList = (props) => {
   const [height, setHeight] = useState(233);
 
   useEffect(() => {
-    setHeight(props.isGroupOpened? 233 : 0);
-  },[props]);
+    setHeight(props.isGroupOpened ? 233 : 0);
+  }, [props]);
+
+  const groupsPresent = props.groups.map((e, idx) =>
+      <GroupContent>
+        <input type="checkbox"
+               onChange={() => props.onGroupCheckboxClicked(props.groups[idx].id)}
+               checked={props.checkedGroups.includes(props.groups[idx].id)}/>
+        {props.groups[idx].name}
+      </GroupContent>
+  );
+
+  useEffect(() => console.log(props.checkedGroups), [props.checkedGroups]);
 
   return (
       <div className="App">
         <Motion style={{height: spring(height)}}>
-          {
-            ({height}) => <div style={Object.assign({}, styles.menu, {height})}>
-              <GroupContent><input type="checkbox"/>하이하이</GroupContent>
-              <GroupContent><input type="checkbox"/>하이하이</GroupContent>
-              <GroupContent><input type="checkbox"/>하이하이</GroupContent>
-              <GroupContent><input type="checkbox"/>하이하이</GroupContent>
-            </div>
-          }
+        {
+          ({height}) => <div style={Object.assign({}, styles.menu, {height})}>
+            {groupsPresent}
+          </div>
+        }
         </Motion>
       </div>
   );
--- frontend/rush/src/components/writing/step3/WritingStep3Modal.js
+++ frontend/rush/src/components/writing/step3/WritingStep3Modal.js
@@ -4,7 +4,24 @@ import GroupList from "./GroupList";
 import GroupListOpenButton from "./GroupListOpenButton";
 
 const WritingStep3Modal = (props) => {
+  const groups = [
+    {id: 1, name: "그룹A"},
+    {id: 3, name: "그룹C"},
+    {id: 4, name: "그룹D"},
+  ];
   const [isGroupOpened, setIsGroupOpened] = useState(false);
+
+  const [isPublicMapChecked, setIsPublicMapChecked] = useState(false);
+  const [isPrivateMapChecked, setIsPrivateMapChecked] = useState(false);
+  const [checkedGroups, setCheckedGroups] = useState([]);
+
+  const onGroupCheckboxClicked = (groupId) => {
+    if (checkedGroups.includes(groupId)) {
+      setCheckedGroups(checkedGroups.filter(e => e !== groupId))
+    } else {
+      setCheckedGroups([...checkedGroups, groupId]);
+    }
+  };
 
   return (
       <Modal            //모달 태그 내부에 옵션을 넣고 태그 사이에 내용을 넣는다
@@ -24,7 +41,26 @@ const WritingStep3Modal = (props) => {
           contentLabel="Example Modal"       //모달의 라벨
       >
         <div>
-           <input type="checkbox"/>전체지도
+          <button onClick={()=>{
+
+            if(isPublicMapChecked || isPrivateMapChecked || checkedGroups.length > 0){
+              setCheckedGroups([]);
+              setIsPublicMapChecked(false);
+              setIsPrivateMapChecked(false);
+            }
+            else {
+              setCheckedGroups(groups.map(group => group.id));
+              setIsPublicMapChecked(true)
+              setIsPrivateMapChecked(true)
+            }
+            }} >전체선택</button>
+        </div>
+        <div>
+           <input type="checkbox"
+                  onChange={() => setIsPublicMapChecked(!isPublicMapChecked)}
+                  checked={isPublicMapChecked}
+           />
+           전체지도
         </div>
         <div>
           그룹지도
@@ -33,9 +69,17 @@ const WritingStep3Modal = (props) => {
               setIsGroupOpened={setIsGroupOpened}
           />
         </div>
-        <GroupList isGroupOpened={isGroupOpened}/>
+        <GroupList
+            isGroupOpened={isGroupOpened}
+            groups={groups}
+            checkedGroups={checkedGroups}
+            onGroupCheckboxClicked={onGroupCheckboxClicked}
+        />
         <div>
-           <input type="checkbox"/>개인지도
+           <input type="checkbox"
+                  onChange={() => setIsPrivateMapChecked(!isPrivateMapChecked)}
+                  checked={isPrivateMapChecked}
+           />개인지도
         </div>
       </Modal>
   );
