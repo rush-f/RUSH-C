72c6e3018b54a20d066f3cbdc7ce102be0cf278c
khdscor <khds_cor@naver.com>
2021-04-12 16:30:15 +0900

    feat: 글상세 페이지 생성 및 라우팅

--- frontend/rush/src/App.js
+++ frontend/rush/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import DefaultMapPage from './components/home/DefaultMapPage';
 import LoginPage from './components/login/LoginPage';
 import WritingPage from './components/writing/WritingPage';
+import PostDetailPage from "./components/postDetail/PostDetailPage";
 
 function App() {
   return (<Router>
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" exact component={DefaultMapPage} />
       <Route path="/login" exact component={LoginPage} />
       <Route path="/writing" exact component={WritingPage} />
+      <Route path="/posts/:postId" exact component={PostDetailPage} />
     </Switch>
   </Router>);
 }
