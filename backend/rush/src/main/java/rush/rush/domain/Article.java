a8eed78065b41234164eb85a9dc97c087202386a
khdscor <khds_cor@naver.com>
2021-04-26 16:49:01 +0900

    feat: Entity 생성, Article 구현

---
+++ backend/rush/src/main/java/rush/rush/domain/Article.java
@@ -0,0 +1,41 @@
+package rush.rush.domain;
+
+import com.sun.istack.NotNull;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Lob
+    @NotNull
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
