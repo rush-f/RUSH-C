f0cd56c3c9fe4f33dcd17e9ce82b3983401e7dfb
김예림 <kiel0103@naver.com>
2021-04-29 16:56:11 +0900

    feat: Article 한 개 조회 기능 추가

---
+++ backend/rush/src/main/java/rush/rush/dto/ArticleResponse.java
@@ -0,0 +1,18 @@
+package rush.rush.dto;
+
+import java.sql.Timestamp;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+public class ArticleResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private Timestamp createDate;
+}
--- backend/rush/src/main/java/rush/rush/service/ArticleService.java
+++ backend/rush/src/main/java/rush/rush/service/ArticleService.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rush.rush.domain.Article;
+import rush.rush.dto.ArticleResponse;
 import rush.rush.dto.CreateArticleRequest;
 import rush.rush.repository.ArticleRepository;
 
@@ -19,5 +20,17 @@ public class ArticleService {
 
         return articleRepository.save(article)
             .getId();
+    }
+
+    public ArticleResponse findOne(Long id) {
+        Article article = articleRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("id가 " + id + "인 article이 없습니다."));
+
+        return new ArticleResponse(
+            article.getId(),
+            article.getTitle(),
+            article.getContent(),
+            article.getCreateDate()
+        );
     }
 }
