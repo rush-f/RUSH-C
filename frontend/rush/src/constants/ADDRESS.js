11e2e4b60cef1b2a09922a19322a6c2ad0654ca5
김예림 <kiel0103@naver.com>
2021-05-26 20:12:15 +0900

    feat: 실서버 주소 적용

---
+++ frontend/rush/src/constants/ADDRESS.js
@@ -0,0 +1,4 @@
+export const BACKEND_ADDRESS = "http://seoultechfootprint.shop:8080";
+export const FRONTEND_ADDRESS = "http://seoultechfootprint.shop:3000";
+// export const BACKEND_ADDRESS = "http://localhost:8080";
+// export const FRONTEND_ADDRESS = "http://localhost:3000";
