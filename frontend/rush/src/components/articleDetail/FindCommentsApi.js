cbfa2dc93ae30ebd806aa528053e8f4445fdaf26
김예림 <kiel0103@naver.com>
2021-05-31 23:10:49 +0900

    feat: 댓글 작성 및 조회 프론트&백엔드 연동

---
+++ frontend/rush/src/components/articleDetail/FindCommentsApi.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import {BACKEND_ADDRESS} from "../../constants/ADDRESS";
+
+const findCommentsApi = (articleId) => {
+  return axios.get(BACKEND_ADDRESS + "/comments?article_id=" + articleId)
+    .then(response => response.data);
+};
+
+export default findCommentsApi;
