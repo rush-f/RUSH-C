d1ee0541b53b60b495e122ce036e2b659f5f3c69
김예림 <kiel0103@naver.com>
2021-06-23 17:23:03 +0900

    feat: mariaDB 의존성 추가

--- backend/rush/src/main/java/rush/rush/RushApplication.java
+++ backend/rush/src/main/java/rush/rush/RushApplication.java
@@ -9,15 +9,8 @@ import rush.rush.security.AppProperties;
 @SpringBootApplication
 public class RushApplication {
 
-    private static final String APPLICATION_LOCATIONS = "spring.config.location="
-        + "classpath:application.yml,"
-        + "classpath:application-oauth.yml,"
-        + "classpath:application-local.yml,"
-        + "optional:/app/config/application-real.yml";
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(RushApplication.class)
-            .properties(APPLICATION_LOCATIONS)
             .run(args);
     }
 }
