deb0bc813044a305c4cd5c393570a486fb5bad4f
김예림 <kiel0103@naver.com>
2021-06-29 00:12:58 +0900

    feat: ArticleGroup 엔티티 구현

---
+++ backend/rush/src/main/java/rush/rush/domain/ArticleGroup.java
@@ -0,0 +1,59 @@
+package rush.rush.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class ArticleGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ARTICLE_ID", nullable = false)
+    private Article article;
+
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID", nullable = false)
+    private Group group;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    public ArticleGroup(Long id, Article article, Group group, Timestamp createDate) {
+        validate(article);
+        validate(group);
+        this.id = id;
+        this.article = article;
+        this.group = group;
+        this.createDate = createDate;
+    }
+    
+    private void validate(Article article) {
+        if (Objects.isNull(article)) {
+            throw new IllegalArgumentException("article이 null일 수 없습니다.");
+        }
+        if (Objects.isNull(article.getId())) {
+            throw new IllegalArgumentException("article에 id가 없습니다.");
+        }
+    }
+    
+    private void validate(Group group) {
+        if (Objects.isNull(group)) {
+            throw new IllegalArgumentException("group이 null일 수 없습니다.");
+        }
+        if (Objects.isNull(group.getId())) {
+            throw new IllegalArgumentException("group에 id가 없습니다.");
+        }
+    }
+}
