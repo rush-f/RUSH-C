f2ae8dca35539a9bd156eb94f6e39c6918aa3220
김예림 <kiel0103@naver.com>
2021-06-30 13:13:10 +0900

    feat: 전체지도 글 보기 구현

--- frontend/rush/src/components/articleDetail/FindWritingApi.js
+++ frontend/rush/src/components/articleDetail/FindWritingApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {BACKEND_ADDRESS} from "../../constants/ADDRESS";
 
 const findWritingApi = (articleId) => {
-  return axios.get(BACKEND_ADDRESS + "/articles/" + articleId)
+  return axios.get(BACKEND_ADDRESS + "/articles/public/" + articleId)
     .then(response => response.data);
 };
 
