df09bd699150eb4b318bcfe1ec672972b37cd0fb
khdscor <khds_cor@naver.com>
2021-07-05 16:15:05 +0900

    feat: 마이페이지 전체적인 틀 마련

---
+++ frontend/rush/src/components/home/button/Menu/Box.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Outside = styled.div`
+  max-width: 400px;
+  margin: auto;
+`;
+
+const DisplayBox = styled.div`
+  margin: 10px;
+  border: 2px solid rgb(90, 155, 213);
+  border-radius: 10px;
+  overflow-y: scroll;
+`;
+
+
+export { Outside, DisplayBox };
--- frontend/rush/src/components/home/button/Menu/MyPage.js
+++ frontend/rush/src/components/home/button/Menu/MyPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Outside,DisplayBox} from "./Box";
+import WindowSize from "../../../WindowSize";
+import CancelButton from "./CancelButton";
 
 const MyPage = () => {
   return (
-      <div>
-        
-      </div>
+      <Outside>
+        <DisplayBox style={{height: WindowSize().height - 50, marginTop: 15}}>
+          <CancelButton></CancelButton>
+        </DisplayBox>
+      </Outside>
   );
 };
 
