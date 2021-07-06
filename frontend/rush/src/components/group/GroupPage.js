24570d637b9840e9081ba9046afd6806a4fee3d5
김예림 <kiel0103@naver.com>
2021-07-06 14:49:57 +0900

    feat: GroupPage 에서 accessToken, groupId 가져오기 구현

--- frontend/rush/src/components/group/GroupPage.js
+++ frontend/rush/src/components/group/GroupPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import WindowSize from "../WindowSize";
 import {DisplayBox, Outside} from "../common/Box";
+import {ACCESS_TOKEN} from "../../constants/SessionStorage";
 
-const GroupPage = () => {
+const GroupPage = (props) => {
+  const accessToken = sessionStorage.getItem(ACCESS_TOKEN);
+  const groupId = props.match.params.groupId;
+
   return (
     <Outside>
       <DisplayBox style={{height: WindowSize().height - 50, marginTop: 15}}>
+        {groupId}
       </DisplayBox>
     </Outside>
   );
