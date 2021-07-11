b4229ea25ccf8e91adb6cdf2cd55a208dbcb26d8
김예림 <kiel0103@naver.com>
2021-07-11 13:49:10 +0900

    refactor: 프론트 홈 패키지 구조 개선

--- frontend/rush/src/components/home/group/GroupList.js
+++ frontend/rush/src/components/home/group/GroupList.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react';
 import {Motion, spring} from 'react-motion';
 import GroupContent from "./GroupContent";
 import styled from "styled-components";
-import findMyGroupsApi from "../../../../../api/FindMyGroupsApi";
+import findMyGroupsApi from "../../../api/FindMyGroupsApi";
 
 const GroupSetting = styled.li`
   height: 50px;
