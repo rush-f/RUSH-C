e45059753dd9343a91ffdca81af7b7b52660f3d4
김예림 <kiel0103@naver.com>
2021-07-19 16:59:14 +0900

    feat: UserRepository findAllByGroupId 구현

--- backend/rush/src/main/java/rush/rush/repository/UserRepository.java
+++ backend/rush/src/main/java/rush/rush/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package rush.rush.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rush.rush.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -9,4 +12,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
+
+    @Query("select user from User user "
+        + "inner join user.userGroups usergroups "
+        + "inner join usergroups.group g "
+        + "where g.id = :groupId")
+    List<User> findAllByGroupId(@Param("groupId") Long groupId);
 }
