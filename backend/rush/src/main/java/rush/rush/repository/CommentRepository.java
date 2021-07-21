7e5afc0b65467f49672fc77f472fadfc48aae36c
김예림 <kiel0103@naver.com>
2021-07-21 22:54:56 +0900

    refactor: CommentRepository findAllOfPrivateArticle 구현

--- backend/rush/src/main/java/rush/rush/repository/CommentRepository.java
+++ backend/rush/src/main/java/rush/rush/repository/CommentRepository.java
@@ -1,9 +1,19 @@
 package rush.rush.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rush.rush.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByArticleIdOrderByCreateDateDesc(Long articleId);
+
+    @Query("select comment from Comment comment "
+        + "inner join comment.article "
+        + "where comment.article.id = :articleId "
+        + "and comment.article.user.id = :userId "
+        + "order by comment.createDate desc")
+    List<Comment> findAllOfPrivateArticle(
+        @Param("articleId") Long articleId, @Param("userId") Long userId);
 }
--- backend/rush/src/test/java/rush/rush/repository/CommentRepositoryTest.java
+++ backend/rush/src/test/java/rush/rush/repository/CommentRepositoryTest.java
@@ -5,6 +5,7 @@ import static rush.rush.repository.SetUpMethods.persistArticle;
 import static rush.rush.repository.SetUpMethods.persistUser;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,7 @@ import rush.rush.domain.Article;
 import rush.rush.domain.Comment;
 import rush.rush.domain.User;
 
-@ExtendWith(SpringExtension.class)  // junit5에게 Spring support를 enable 하라고 말하는거
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class CommentRepositoryTest {
 
@@ -44,6 +45,8 @@ class CommentRepositoryTest {
 
         // when
         List<Comment> comments = commentRepository.findAllByArticleIdOrderByCreateDateDesc(article.getId());
+
+        // then
         assertThat(comments).isNotNull();
         assertThat(comments).hasSize(2);
         assertThat(comments.get(0).getContent()).isEqualTo(COMMENT_CONTENT);
@@ -51,5 +54,52 @@ class CommentRepositoryTest {
         assertThat(comments.get(0).getArticle().getId()).isEqualTo(article.getId());
         assertThat(comments.get(0).getCreateDate()).isEqualTo(comment2.getCreateDate());
         assertThat(comments.get(1).getCreateDate()).isEqualTo(comment1.getCreateDate());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("개인지도 댓글 조회")
+    void findAllOfPrivateArticle() {
+        // given
+        User user = persistUser(testEntityManager, "test@email.com");
+
+        Article article = persistArticle(testEntityManager,
+            user, false, true, 37.14, 34.24);
+
+        Comment comment1 = new Comment(COMMENT_CONTENT, user, article);
+        testEntityManager.persist(comment1);
+        Comment comment2 = new Comment(COMMENT_CONTENT, user, article);
+        testEntityManager.persist(comment2);
+
+        // when
+        List<Comment> comments = commentRepository.findAllOfPrivateArticle(article.getId(), user.getId());
+
+        // then
+        assertThat(comments).isNotNull();
+        assertThat(comments).hasSize(2);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("개인지도 댓글 조회 - 내 글이 아닌 개인지도 글 조회 시도시 조회되지 않음")
+    void findAllOfPrivateArticle_IfNotHaveAuth_SizeIsZero() {
+        // given
+        User user = persistUser(testEntityManager, "test1@email.com");
+        User another = persistUser(testEntityManager, "test2@email.com");
+
+        Article article = persistArticle(testEntityManager,
+            user, false, true, 37.14, 34.24);
+
+        Comment comment1 = new Comment(COMMENT_CONTENT, user, article);
+        testEntityManager.persist(comment1);
+        Comment comment2 = new Comment(COMMENT_CONTENT, user, article);
+        testEntityManager.persist(comment2);
+
+        // when
+        List<Comment> comments = commentRepository
+            .findAllOfPrivateArticle(article.getId(), another.getId());
+
+        // then
+        assertThat(comments.isEmpty()).isTrue();
     }
 }
