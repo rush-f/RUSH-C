0817aede16c42776ab1a6e4c29046ecf3cd8ca23
김예림 <kiel0103@naver.com>
2021-07-21 15:06:12 +0900

    refactor: CommentController를 더 작은 단위로 쪼갬

---
+++ backend/rush/src/main/java/rush/rush/controller/CreateCommentController.java
@@ -0,0 +1,34 @@
+package rush.rush.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import rush.rush.dto.CommentResponse;
+import rush.rush.dto.CreateCommentRequest;
+import rush.rush.security.CurrentUser;
+import rush.rush.security.user.UserPrincipal;
+import rush.rush.service.CommentService;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+public class CreateCommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentResponse> create(@RequestParam(value = "article_id") Long articleId,
+        @RequestBody CreateCommentRequest createCommentRequest,
+        @CurrentUser UserPrincipal userPrincipal) {
+        CommentResponse commentResponse =
+            commentService.create(articleId, createCommentRequest, userPrincipal.getUser());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(commentResponse);
+    }
+}
