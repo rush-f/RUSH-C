eedc1318b72316b2366d9bec6eb0869b4cbe6470
khdscor <khds_cor@naver.com>
2021-07-22 18:26:50 +0900

    refactor: 프론트 - 피드백 반영, api 및 코드 간소화

--- frontend/rush/src/api/CheckHasIlikedApi.js
+++ frontend/rush/src/api/CheckHasIlikedApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import {BACKEND_ADDRESS} from "../constants/ADDRESS";
 
-const checkArticleMyLikingApi = (accessToken, articleId) => {
+const checkHasIlikedApi = (accessToken, articleId) => {
   const config = {
     headers: {
       "Authorization": "Bearer " + accessToken
     }
   };
-  return axios.get(BACKEND_ADDRESS + "/like/article?article_id="+articleId, config)
+  return axios.get(BACKEND_ADDRESS + "/articles/"+articleId+"/like", config)
   .then(response => response.data);
 };
 
-export default checkArticleMyLikingApi;
+export default checkHasIlikedApi;
--- frontend/rush/src/components/articleDetail/ArticleBody.js
+++ frontend/rush/src/components/articleDetail/ArticleBody.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ArticleTitle from "./ArticleTitle";
 import styled from "styled-components";
-import addArticleLikingApi from "../../api/AddArticleLikingApi";
-import deleteArticleLikingApi from "../../api/DeleteArticleLikingApi";
+import changeMyLikingApi from "../../api/ChangeMyLikingApi";
 
 const ArticleContent = styled.div`
   font-size: 24px;
   margin-top: 10px;
-`;
+`
 
 const ArticleLiking = styled.div`
   display: flex;
-`;
+`
 
 const LikingHeart = styled.div`
   font-size: 40px;
@@ -25,7 +24,7 @@ const LikingHeart = styled.div`
   -khtml-user-select: none;
   -webkit-user-select: none;
   user-select: none;
- `;
+ `
 
 const LikingLetter = styled.div`
   font-size: 17px;
@@ -45,18 +44,12 @@ const ArticleBody = (props) => {
         <ArticleLiking>
           <LikingHeart
             onClick={() => {
-              if(props.checkMyLiking){
-                deleteArticleLikingApi(props.accessToken, props.articleId, props.history);
-                props.setArticleTotalLiking(props.articleTotalLiking-1);
-                props.setCheckMyLiking(false);
+              props.setArticleTotalLikes(props.hasILiked?props.articleTotalLikes-1 : props.articleTotalLikes+1);
+              changeMyLikingApi(props.accessToken, props.hasILiked, props.articleId, props.history);
+              props.setHasILiked(!props.hasILiked);
               }
-              else{
-                addArticleLikingApi(props.accessToken, props.articleId, props.history);
-                props.setArticleTotalLiking(props.articleTotalLiking+1);
-                props.setCheckMyLiking(true);
-              }
-            }}>{props.checkMyLiking?"♥":"♡"} </LikingHeart>
-          <LikingLetter>좋아요 {props.article ? props.articleTotalLiking : ""}개</LikingLetter>
+            }>{props.hasILiked?"♥":"♡"} </LikingHeart>
+          <LikingLetter>좋아요 {props.article ? props.articleTotalLikes : ""}개</LikingLetter>
         </ArticleLiking>
       </div>
   );
