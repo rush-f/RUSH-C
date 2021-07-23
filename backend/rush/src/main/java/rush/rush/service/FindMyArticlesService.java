68d0fc62baea0c7a05b3e39b1e42cdd82a48ce37
khdscor <khds_cor@naver.com>
2021-07-23 22:04:48 +0900

    refactor: 수정한 repository에 맞게 service 수정

--- backend/rush/src/main/java/rush/rush/service/FindMyArticlesService.java
+++ backend/rush/src/main/java/rush/rush/service/FindMyArticlesService.java
@@ -6,11 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rush.rush.domain.Article;
-import rush.rush.domain.ArticleGroup;
-import rush.rush.domain.Group;
 import rush.rush.dto.GroupSummaryResponse;
 import rush.rush.dto.MyPageArticleResponse;
-import rush.rush.repository.ArticleGroupRepository;
 import rush.rush.repository.ArticleRepository;
 
 @Service
@@ -18,36 +15,29 @@ import rush.rush.repository.ArticleRepository;
 public class FindMyArticlesService {
 
     final private ArticleRepository articleRepository;
-    final private ArticleGroupRepository articleGroupRepository;
     
     @Transactional
     public List<MyPageArticleResponse> findMyArticles(Long userId) {
 
-        List<Article> articles = articleRepository.findAllByUserId(userId);
+        List<Article> articles = articleRepository.findArticlesWithGroupsByUserId(userId);
 
         return articles.stream()
-            .map(article ->{
-                List<GroupSummaryResponse> groups = findAllByArticle(article);
-                return new MyPageArticleResponse(
-                    article.getId(),
-                    article.getTitle(),
-                    article.isPublicMap(),
-                    article.isPrivateMap(),
-                    groups);
-            })
+            .map(article -> new MyPageArticleResponse(
+                article.getId(),
+                article.getTitle(),
+                article.isPublicMap(),
+                article.isPrivateMap(),
+                findAllByArticle(article)
+             ))
             .collect(Collectors.toUnmodifiableList());
     }
 
-    @Transactional
-    public List<GroupSummaryResponse> findAllByArticle(Article article) {
-
-        List<ArticleGroup> articleGroups = articleGroupRepository.findAllByArticleId(article.getId());
-
-        return articleGroups.stream()
-            .map(articleGroup -> {
-                Group group = articleGroup.getGroup();
-                return new GroupSummaryResponse(group.getId(), group.getName());
-            })
-            .collect(Collectors.toUnmodifiableList());
+    private List<GroupSummaryResponse> findAllByArticle(Article article){
+        return article.getArticleGroups().stream()
+            .map((articleGroup)-> new GroupSummaryResponse(
+                articleGroup.getGroup().getId(),
+                articleGroup.getGroup().getName()
+            ))
+            .collect(Collectors.toList());
     }
 }
--- frontend/rush/src/constants/ADDRESS.js
+++ frontend/rush/src/constants/ADDRESS.js
@@ -1,4 +1,4 @@
-//export const BACKEND_ADDRESS = "http://seoultechfootprint.shop:8080";
-//export const FRONTEND_ADDRESS = "http://seoultechfootprint.shop:80";
-export const BACKEND_ADDRESS = "http://localhost:8080";
-export const FRONTEND_ADDRESS = "http://localhost:3000";
+export const BACKEND_ADDRESS = "http://seoultechfootprint.shop:8080";
+export const FRONTEND_ADDRESS = "http://seoultechfootprint.shop:80";
+//export const BACKEND_ADDRESS = "http://localhost:8080";
+//export const FRONTEND_ADDRESS = "http://localhost:3000";
