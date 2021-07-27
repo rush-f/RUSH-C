fa0bc7ff2ac272f06588d5000c1e91335d950223
김예림 <kiel0103@naver.com>
2021-07-27 14:15:39 +0900

    feat: 실서버 백엔드 주소 https 로 변경

--- frontend/rush/src/constants/ADDRESS.js
+++ frontend/rush/src/constants/ADDRESS.js
@@ -1,4 +1,4 @@
-export const BACKEND_ADDRESS = "http://seoultechfootprint.shop:80/api";
-export const FRONTEND_ADDRESS = "http://seoultechfootprint.shop:80";
+export const BACKEND_ADDRESS = "https://seoultechfootprint.shop/api";
+export const FRONTEND_ADDRESS = "https://seoultechfootprint.shop";
 // export const BACKEND_ADDRESS = "http://localhost:8080/api";
 // export const FRONTEND_ADDRESS = "http://localhost:3000";
