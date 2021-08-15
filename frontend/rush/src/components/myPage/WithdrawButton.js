1d236fe3a9ae0d34bee30bd49052d5cc024fafee
khdscor <khds_cor@naver.com>
2021-08-15 18:42:50 +0900

    refactor: 프론트 디자인 수정

--- frontend/rush/src/components/myPage/WithdrawButton.js
+++ frontend/rush/src/components/myPage/WithdrawButton.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 const WithdrawButtonStyle = styled.div`
   color: #777777;
-  display: inline-block;
+  margin: 30px 14px;
   cursor: pointer;
 `;
 
@@ -13,16 +13,16 @@ const WithdrawButton = ({nickName, accessToken, history}) => {
     withdrawUserApi({accessToken, history})
   };
   return (
-      <WithdrawButtonStyle onClick={()=>{
-        if (window.confirm("정말 회원탈퇴를 진행하시겠습니까?")){    //확인
-          if (window.confirm("회원을 탈퇴하면 작성한 모드 게시글 및 댓글이 삭제됩니다. 계속 진행하시겠습니까?")){
-            if(window.prompt("자신의 회원이름을 입력해주세요") === nickName){
-              onWithdrawButtonClicked()
-            } else { alert("회원이름을 잘못입력하였습니다")}
-          }
-        }}}>
-        삭제
-      </WithdrawButtonStyle>
+        <WithdrawButtonStyle onClick={()=>{
+          if (window.confirm("정말 회원탈퇴를 진행하시겠습니까?")){    //확인
+            if (window.confirm("회원을 탈퇴하면 작성한 모드 게시글 및 댓글이 삭제됩니다. 계속 진행하시겠습니까?")){
+              if(window.prompt("자신의 회원이름을 입력해주세요") === nickName){
+                onWithdrawButtonClicked()
+              } else { alert("회원이름을 잘못입력하였습니다")}
+            }
+          }}}>
+          회원 탈퇴
+        </WithdrawButtonStyle>
   );
 };
 
