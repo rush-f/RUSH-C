47f476db589f4a6aa9bd0bcf3f174b629aaccd0a
yelim980103 <yelim980103@gmail.com>
2021-08-16 15:11:46 +0900

    refactor: DeleteCommentServiceTest setUp 메서드 추출

--- backend/rush/src/test/java/rush/rush/service/DeleteCommentServiceTest.java
+++ backend/rush/src/test/java/rush/rush/service/DeleteCommentServiceTest.java
@@ -3,6 +3,7 @@ package rush.rush.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,62 +33,22 @@ class DeleteCommentServiceTest {
     @Autowired
     private DeleteCommentService deleteCommentService;
 
-    @Test
-    @Transactional
-    @DisplayName("댓글 삭제")
-    void deleteComment() {
-        // given : 글에 댓글이 한개 저장되어있다.
-        User savedUser = userRepository.save(
+    private User articleAuthor;
+    private Article article;
+    private User commentAuthor;
+    private Comment comment;
+
+    @BeforeEach
+    void setUp() {
+        articleAuthor = userRepository.save(
             User.builder()
-                .email("test@test.com")
+                .email("articleAuthor@test.com")
                 .password("test password")
                 .nickName("test")
                 .provider(AuthProvider.local)
                 .build()
         );
-
-        Article savedArticle = articleRepository.save(
-            Article.builder()
-                .title("title")
-                .content("content")
-                .latitude(0.0)
-                .longitude(0.0)
-                .user(savedUser)
-                .privateMap(true)
-                .publicMap(true)
-                .build()
-        );
-
-        Comment comment = commentRepository.save(
-            Comment.builder()
-                .user(savedUser)
-                .content("댓글내용")
-                .article(savedArticle)
-                .build()
-        );
-
-        // when : 댓글을 삭제한다.
-        deleteCommentService.deleteComment(comment.getId(), savedUser);
-
-        // then : 댓글이 삭제되었다.
-        assertThat(commentRepository.findAllOfPublicArticle(savedArticle.getId()))
-            .hasSize(0);
-    }
-
-    @Test
-    @Transactional
-    @DisplayName("댓글 삭제 - 본인 댓글이 아닌데 삭제 시도시 예외처리")
-    void deleteComment_IfNotAuthor_ThrowException() {
-        // given : 글에 댓글이 한개 저장되어있다.
-        User articleAuthor = userRepository.save(
-            User.builder()
-                .email("articleAuthor@test.com")
-                .password("test password")
-                .nickName("articleAuthor")
-                .provider(AuthProvider.local)
-                .build()
-        );
-        Article savedArticle = articleRepository.save(
+        article = articleRepository.save(
             Article.builder()
                 .title("title")
                 .content("content")
@@ -98,27 +59,45 @@ class DeleteCommentServiceTest {
                 .publicMap(true)
                 .build()
         );
-        User commentAuthor = userRepository.save(
+        commentAuthor = userRepository.save(
             User.builder()
                 .email("commentAuthor@test.com")
                 .password("test password")
-                .nickName("commentAuthor")
+                .nickName("test")
                 .provider(AuthProvider.local)
                 .build()
         );
-        Comment comment = Comment.builder()
-            .user(commentAuthor)
-            .content("댓글내용")
-            .article(savedArticle)
-            .build();
-        commentRepository.save(comment);
+        comment = commentRepository.save(
+            Comment.builder()
+                .user(commentAuthor)
+                .content("댓글내용")
+                .article(article)
+                .build()
+        );
+    }
 
+    @Test
+    @Transactional
+    @DisplayName("댓글 삭제")
+    void deleteComment() {
+        // when : 댓글을 삭제한다.
+        deleteCommentService.deleteComment(comment.getId(), commentAuthor);
+
+        // then : 댓글이 삭제되었다.
+        assertThat(commentRepository.findAllOfPublicArticle(article.getId()))
+            .hasSize(0);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("댓글 삭제 - 본인 댓글이 아닌데 삭제 시도시 예외처리")
+    void deleteComment_IfNotAuthor_ThrowException() {
         // when & then : 댓글 작성자가 아닌 사람이 댓글 삭제 시도 -> 댓글 삭제 실패
         assertThatThrownBy(() -> deleteCommentService.deleteComment(comment.getId(), articleAuthor))
             .isInstanceOf(IllegalArgumentException.class);
 
         // then : 댓글이 삭제되지 않았다.
-        assertThat(commentRepository.findAllOfPublicArticle(savedArticle.getId()))
+        assertThat(commentRepository.findAllOfPublicArticle(article.getId()))
             .hasSize(1);
     }
 }
