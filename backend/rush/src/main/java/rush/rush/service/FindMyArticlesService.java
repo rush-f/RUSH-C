fce6016c1a42ae98a41927bd5c363e43130a6f80
khdscor <khds_cor@naver.com>
2021-08-23 15:14:09 +0900

    refactor: repository 함수에 맞게 service 수정

--- backend/rush/src/main/java/rush/rush/service/FindMyArticlesService.java
+++ backend/rush/src/main/java/rush/rush/service/FindMyArticlesService.java
@@ -1,11 +1,9 @@
 package rush.rush.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import rush.rush.domain.Article;
 import rush.rush.dto.MyPageArticleResponse;
 import rush.rush.repository.ArticleRepository;
 
@@ -17,17 +15,6 @@ public class FindMyArticlesService {
 
     @Transactional
     public List<MyPageArticleResponse> findMyArticles(Long userId) {
-        List<Article> articles = articleRepository.findArticlesWithCommentsByUserId(userId);
-
-        return articles.stream()
-            .map(article -> new MyPageArticleResponse(
-                article.getId(),
-                article.getTitle(),
-                article.isPublicMap(),
-                article.isPrivateMap(),
-                article.getCreateDate(),
-                article.getComments().stream().count()
-            ))
-            .collect(Collectors.toUnmodifiableList());
+        return articleRepository.findArticlesWithCommentsAndLikes(userId);
     }
 }
