05039f49c80025c881a4ec5a8fe59b82299a13b8
khdscor <khds_cor@naver.com>
2021-09-17 23:06:43 +0900

    refactor: 그룹 및 유저 repository에 QueryHint를 통해 readOnly 적용

--- backend/rush/src/main/java/rush/rush/repository/GroupRepository.java
+++ backend/rush/src/main/java/rush/rush/repository/GroupRepository.java
@@ -2,9 +2,11 @@ package rush.rush.repository;
 
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import rush.rush.domain.Group;
 
@@ -16,12 +18,14 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
         + "inner join g.userGroups usergroup "
         + "inner join usergroup.user user "
         + "where user.id = :userId and g.id = :groupId")
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
     Optional<Group> findByGroupIdAndUserId(@Param("groupId") Long groupId, @Param("userId") Long userId);
 
     @Query("select distinct g from Group g "
         + "inner join g.userGroups usergroup "
         + "inner join usergroup.user user "
         + "where user.id = :userId")
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
     List<Group> findAllByUserId(@Param("userId") Long userId);
 
     @Modifying
