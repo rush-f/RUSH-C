cf54ce355ce82886f892e185c2f613c09d0b4dc0
khdscor <khds_cor@naver.com>
2021-09-29 21:33:58 +0900

    faet: 전역적으로 예외를 처리하도록 ExceptionHandler 구현(service내의 예외만 포함)

---
+++ backend/rush/src/main/java/rush/rush/controller/ExceptionAdvice.java
@@ -0,0 +1,67 @@
+package rush.rush.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import rush.rush.dto.ErrorResponse;
+import rush.rush.exception.AlreadyExistException;
+import rush.rush.exception.AlreadySignedUpException;
+import rush.rush.exception.NotAuthorizedOrExistException;
+import rush.rush.exception.NotExistsException;
+import rush.rush.exception.NotIncludedMapException;
+import rush.rush.exception.WrongGroupIdException;
+import rush.rush.exception.WrongMapTypeException;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(AlreadyExistException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyExistException(AlreadyExistException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(AlreadySignedUpException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadySignedUpException(AlreadySignedUpException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(NotAuthorizedOrExistException.class)
+    public ResponseEntity<ErrorResponse> handleNotAuthorizedOrExistException(NotAuthorizedOrExistException e) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(NotExistsException.class)
+    public ResponseEntity<ErrorResponse> handleNotExistsException(NotExistsException e) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(NotIncludedMapException.class)
+    public ResponseEntity<ErrorResponse> handleNotIncludedMapException(NotIncludedMapException e) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(WrongGroupIdException.class)
+    public ResponseEntity<ErrorResponse> handleWrongGroupIdException(WrongGroupIdException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(WrongMapTypeException.class)
+    public ResponseEntity<ErrorResponse> handleWrongMapTypeException(WrongMapTypeException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+}
