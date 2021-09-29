e6b2e25a685e7a85bed4d699f31c7cddea7d2f65
khdscor <khds_cor@naver.com>
2021-09-29 21:32:37 +0900

    faet: 예외 발생시 클라이언트로 전달할 ErrorResponse 구현

---
+++ backend/rush/src/main/java/rush/rush/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package rush.rush.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+    private String errorMessage;
+}
