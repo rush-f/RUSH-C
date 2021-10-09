53ea89ef377722ea4fa8cc0bdeee9b4a496ce2f2
yelim980103 <yelim980103@gmail.com>
2021-10-09 15:15:24 +0900

    test: CreatePublicMapArticleTest - 토큰이 잘못된 경우 401 응답

--- backend/rush/src/test/java/rush/rush/acceptance/article/CreatePublicMapArticleTest.java
+++ backend/rush/src/test/java/rush/rush/acceptance/article/CreatePublicMapArticleTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -144,7 +145,34 @@ public class CreatePublicMapArticleTest {
         assertThat(savedArticle.getTotalLikes()).isZero();
     }
 
+    @ParameterizedTest
+    @DisplayName("전체지도에만 글쓰기 - 토큰이 잘못된 경우 401 Unauthorized 응답")
+    @ValueSource(strings = {"", "말도 안되는 토큰"})
+    void createPublicArticle_IfTokenIsWrong_Response401(String wrongToken) {
+        final String title = "글제목";
+        final String content = "글내용";
+        final double latitude = 45.0;
+        final double longitude = 132.0;
 
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("title", title);
+        body.put("content", content);
+        body.put("latitude", latitude);
+        body.put("longitude", longitude);
+        body.put("publicMap", true);
+        body.put("privateMap", false);
+
+        given()
+            .header("Authorization", "Bearer " + wrongToken)
+            .body(body)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+        .when()
+            .post("/api/articles")
+        .then()
+            .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
 
     private Long extractArticleIdFrom(String locationHeader) {
         String[] split = locationHeader.split("/");
