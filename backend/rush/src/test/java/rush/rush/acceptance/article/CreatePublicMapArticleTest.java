7631227cf31dd9da8c9d2452486e5d7b228bd9c0
yelim980103 <yelim980103@gmail.com>
2021-10-09 14:00:00 +0900

    test: CreatePublicMapArticleTest setUp 리팩토링

--- backend/rush/src/test/java/rush/rush/acceptance/article/CreatePublicMapArticleTest.java
+++ backend/rush/src/test/java/rush/rush/acceptance/article/CreatePublicMapArticleTest.java
@@ -1,20 +1,15 @@
 package rush.rush.acceptance.article;
 
-import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
-import rush.rush.dto.AuthResponse;
+import rush.rush.acceptance.setup.AuthSetUp;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
@@ -33,48 +28,13 @@ public class CreatePublicMapArticleTest {
         final String email = "test@email.com";
         final String password = "password";
 
-        signUp(nickName, email, password);
-        token = login(email, password);
+        AuthSetUp.signUp(nickName, email, password);
+        token = AuthSetUp.login(email, password);
     }
 
     @Test
     @DisplayName("전체지도에 글쓰기")
     void createPublicArticle() {
         assertThat(token).isNotNull();
-    }
-
-    private void signUp(String nickName, String email, String password) {
-        Map<String, String> body = new HashMap<>();
-        body.put("nickName", nickName);
-        body.put("email", email);
-        body.put("password", password);
-
-        given()
-            .body(body)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-        .when()
-            .post("/api/auth/signup")
-        .then()
-            .statusCode(HttpStatus.CREATED.value());
-    }
-
-    private String login(String email, String password) {
-        Map<String, String> body = new HashMap<>();
-        body.put("email", email);
-        body.put("password", password);
-
-        AuthResponse authResponse = given()
-            .body(body)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/auth/login")
-            .then()
-            .extract()
-            .body()
-            .as(AuthResponse.class);
-
-        return authResponse.getAccessToken();
     }
 }
---
+++ backend/rush/src/test/java/rush/rush/acceptance/setup/AuthSetUp.java
@@ -0,0 +1,47 @@
+package rush.rush.acceptance.setup;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import rush.rush.dto.AuthResponse;
+
+public class AuthSetUp {
+
+    public static void signUp(String nickName, String email, String password) {
+        Map<String, String> body = new HashMap<>();
+        body.put("nickName", nickName);
+        body.put("email", email);
+        body.put("password", password);
+
+        given()
+            .body(body)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+        .post("/api/auth/signup")
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
+    }
+
+    public static String login(String email, String password) {
+        Map<String, String> body = new HashMap<>();
+        body.put("email", email);
+        body.put("password", password);
+
+        AuthResponse authResponse = given()
+            .body(body)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/api/auth/login")
+            .then()
+            .extract()
+            .body()
+            .as(AuthResponse.class);
+
+        return authResponse.getAccessToken();
+    }
+}
