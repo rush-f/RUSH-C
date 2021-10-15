75b5292a0ff300cfb60b7125bc4ab93b5fc14d5d
khdscor <khds_cor@naver.com>
2021-10-15 17:12:46 +0900

    test: CreateArticleService 테스트 보완

--- backend/rush/src/test/java/rush/rush/service/article/CreateArticleServiceTest.java
+++ backend/rush/src/test/java/rush/rush/service/article/CreateArticleServiceTest.java
@@ -1,6 +1,7 @@
 package rush.rush.service.article;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import rush.rush.domain.AuthProvider;
 import rush.rush.domain.User;
 import rush.rush.dto.CreateArticleRequest;
+import rush.rush.exception.NotIncludedMapException;
 import rush.rush.repository.UserRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,17 +27,25 @@ class CreateArticleServiceTest {
     @Transactional
     void create() {
         // given
-        User user = User.builder()
-            .email("test@test.com")
-            .password("test password")
-            .nickName("test")
-            .provider(AuthProvider.local)
-            .build();
-        User savedUser = userRepository.save(user);
+        User user = userRepository.save(
+            User.builder()
+                .email("test@test.com")
+                .password("test password")
+                .nickName("test")
+                .provider(AuthProvider.local)
+                .build()
+        );
 
-        // when
+        // when & then
         CreateArticleRequest createArticleRequest = new CreateArticleRequest(
             "af", "sdf", 0, 0, true, true, null);
-        assertThat(createArticleService.create(createArticleRequest, savedUser)).isNotNull();
+        assertThat(createArticleService.create(createArticleRequest, user)).isNotNull();
+
+        // when & then   어느지도에도 속하지 않는 경우
+        CreateArticleRequest wrongRequest = new CreateArticleRequest(
+            "af", "sdf", 0, 0, false, false, null);
+        assertThatThrownBy(
+            () -> createArticleService.create(wrongRequest, user))
+            .isInstanceOf(NotIncludedMapException.class);
     }
 }
