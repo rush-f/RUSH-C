7273272718e4c4e2bdce750a282fc6677ddba80f
khdscor <khds_cor@naver.com>
2021-10-20 10:56:07 +0900

    faet: EditArticleController 구현 및 http 테스트

---
+++ backend/rush/src/main/java/rush/rush/dto/EditArticleContentRequest.java
@@ -0,0 +1,14 @@
+package rush.rush.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+public class EditArticleContentRequest {
+
+    private String newContent;
+}
